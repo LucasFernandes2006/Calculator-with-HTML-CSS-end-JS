5940e4e98eeb5f5cefc8749f4f07377a602c5326
LucasFernandes2006 <lucasdasilvafernandes27@gmail.com>
2024-01-12 17:33:00 -0300

    More Improvements

--- src/script.js
+++ src/script.js
@@ -1,161 +1,184 @@
-  
+/*variable to obtain the values ​​of the input text*/ 
+var num = window.document.getElementById('numer_txt') 
 
-var num = window.document.getElementById('numer_txt')
+/*variable that stores the value of the first number*/ 
 var n1 = ""
-var n2 = ""
-var resu  = ""
-var conta = ""
-num.value = ""
 
+/*variable that stores the value of the second number */
+var n2 = ""
+
+/*variable that stores the result of the account */
+var resu  = ""
+
+/*variable that stores the result of the account with a comma */
+var final = "" 
+
+/*variable that checks the type of account */
+var account = ""
+
+/* function that adds the number 7 to the text input */
 function num7(){
     
-   
   num.value = num.value + "7"
  
-   
  } 
 
+ /* function that adds the number 8 to the text input */
 function num8(){
     
-   
    num.value = num.value + "8"
   
  } 
 
+ /* function that adds the number 9 to the text input */
  function num9(){
     
-   
   num.value = num.value + "9"
  
 }
 
-
+/* function that adds the number 4 to the text input */
 function num4(){
     
-   
   num.value = num.value + "4"
  
 } 
 
+/* function that adds the number 5 to the text input */
 function num5(){
     
-   
   num.value = num.value + "5"
  
 } 
 
+/* function that adds the number 6 to the text input */
 function num6(){
     
-   
   num.value = num.value + "6"
  
 } 
 
+/* function that adds the number 1 to the text input */
 function num1(){
     
-   
   num.value = num.value + "1"
  
 } 
 
+/* function that adds the number 2 to the text input */
 function num2(){
     
-   
   num.value = num.value + "2"
  
 } 
 
+/* function that adds the number 3 to the text input */
 function num3(){
     
-   
   num.value = num.value + "3"
  
 } 
 
+/* function that adds the number 0 to the text input */
 function num0(){
     
-   
   num.value = num.value + "0"
- 
+
 } 
+
+/* function that adds the comma to the text input */
+function comma(){
+      
+  num.value = num.value + ","
+  
+}
  
+/* function that checks whether the sum button has been pressed */
+function sum(){
 
-function soma(){
-
-   n1 =  num.value
+   n1 =  num.value.replace(",", ".")
    num.value = ""
-   conta ="soma"
+   account ="sum"
  }
 
+ /* function that checks if the minus button was pressed */
  function sub(){
 
-  n1 =  num.value
+  n1 =  num.value.replace(",", ".")
   num.value = ""
-  conta ="subtrair"
+  account ="subtraction"
 }
 
+/* function that checks whether the multiplication button has been pressed */
 function mult(){
 
-  n1 =  num.value
+  n1 =  num.value.replace(",", ".")
   num.value = ""
-  conta ="multiplicar"
+  account ="multiplication"
 }
 
+/* function that checks whether the split button has been pressed */
 function divi(){
 
- n1 =  num.value
+ n1 =  num.value.replace(",", ".")
  num.value = ""
- conta ="dividir"
+ account ="division "
 }
 
 
-
+/* function responsible for the account result */
 function result(){
 
-   n2 =  num.value 
+   n2 =  num.value.replace(",", ".")
   
-   if (conta == "soma"){
+   /* if that performs the sum */
+   if (account == "sum"){
 
+    
     resu = Number(n1) + Number(n2)
     num.value = resu
+    final = num.value.replace(".", ",")
+    num.value = final 
   
   }
 
-  if (conta == "subtrair"){
+  /* if that performs the subtraction */
+  if (account == "subtraction"){
 
     resu = Number(n1) - Number(n2)
     num.value = resu
+    final = num.value.replace(".", ",")
+    num.value = final 
   
   }
 
-  if (conta == "multiplicar"){
+  /* if that performs the multiplication */
+  if (account == "multiplication"){
 
     resu = Number(n1) * Number(n2)
     num.value = resu
+    final = num.value.replace(".", ",")
+    num.value = final 
   
   }
 
-  if (conta == "dividir"){
+  /* if that performs the division */
+  if (account == "division "){
 
     resu = Number(n1) / Number(n2)
     num.value = resu
+    final = num.value.replace(".", ",")
+    num.value = final 
   
   }
  
 }
 
-function limpar(){
+/* cleaning button function */
+function clean(){
 
   num.value= ""
   n1 = ""
   n2 = ""
 
-
-
 }
-
-
-
-
-
- 
